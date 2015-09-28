1bd6083f1a58ed2330a808c528599b05f7dc13e0
WagnerGascho <wgascho@uol.com.br>
2015-09-28 19:43:23 -0300

    SeletordeArquivoFix
    changed the return type for the file selector

--- src/formais152/Modelo/SeletordeArquivo.java
+++ src/formais152/Modelo/SeletordeArquivo.java
@@ -9,17 +9,13 @@ import org.eclipse.swt.widgets.Shell;
 
 public class SeletordeArquivo {
 
-	static public File selectFile() {
+	static public String selectFile() {
 		Shell shell = new Shell(new Display());
 		FileDialog dialog = new FileDialog(shell, SWT.NULL);
 		
 		String path = dialog.open();
-		
-		File file = null;
-		if (path != null) {
-			file = new File(path);
-		}
-		return file;
+	
+		return path;
 	}
 
 }
