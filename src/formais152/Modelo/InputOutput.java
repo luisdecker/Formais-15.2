944a0aacdea6341e649867c3b77efbf607a1ac85
WagnerGascho <wgascho@uol.com.br>
2015-09-28 19:55:53 -0300

    Input commit
    input commit

---
+++ src/formais152/Modelo/InputOutput.java
@@ -0,0 +1,70 @@
+package formais152.Modelo;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class InputOutput {
+	
+	static public Automato createAutomato(String location){
+		
+		return null;
+	}
+	
+    static public Gramatica criarGramatica(String location){
+    	BufferedReader br = null;
+	
+    	Gramatica gr = new Gramatica();
+    	try {
+    		br= new BufferedReader(new FileReader(location));
+    	
+    	    String fullline = br.readLine();
+
+    	    ///inicio da criação
+    	    while (fullline != null) {
+    	       String line = fullline;
+
+    	        if( line.contains("->") ){
+    	        	
+    	        	int position = line.indexOf("->");
+    	        	String vn = line.substring(0, position);
+    	        	 line= line.substring(position+2);
+    	        	
+    	        	vn=vn.trim();
+    	        	do{
+    	        		String vt= line.trim();
+    	        		
+    	        		if(line.contains("|")){
+    	        			int nextPosition=line.indexOf("|");
+    	        			
+    	        			
+    	        			vt= line.substring(0,nextPosition);
+    	        			
+    	        			line = line.substring( nextPosition +1 );	
+    	        		}
+    	        		vt = vt.trim();
+    	        		//limpando *
+    	        		if(vn.contains("*")){
+    	        			vn= vn.substring(1);
+    	        		}
+    	        		
+    	        		if(! (vn.length()==0 || vt.length()==0)){
+    	        			gr.adicionaProducao(vn,vt);
+    	        		}
+    	        		
+    	     	
+    	        	}while(line.contains("|"));
+ 	
+    	        }
+    
+    	        fullline =  br.readLine();
+    	    }
+   
+    	    br.close();
+    	} catch (IOException e) {		
+			e.printStackTrace();
+		} 
+    	return gr;
+    }
+
+}
