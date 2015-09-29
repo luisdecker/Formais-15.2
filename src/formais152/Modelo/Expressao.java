7fbaab527438ef33690af1d75e84ee91264a582a
WagnerGascho <wgascho@uol.com.br>
2015-09-29 14:10:05 -0300

    expressao para Automato feito
    Algoritmo não foi testado ainda, esperando concatenação de automatos pra
    poder testar

--- src/formais152/Modelo/Expressao.java
+++ src/formais152/Modelo/Expressao.java
@@ -34,18 +34,27 @@ public class Expressao {
 		}
 		result += ')';
 		System.out.println(result);
-		return auto;
+		return obterAutomato(result,'\0');
 	}
 
 	private boolean isMod(char a) {
 		return (a == '+' || a == '*' || a == '?');
 	}
 
-	public Automato obterAutomato(String express, char mod) {
+	private Automato obterAutomato(String express, char mod) {
 		int vn = 0;
 		Automato auto = new Automato();
 		boolean start = false;
-
+		auto.addEstado("S0");
+		
+		try {
+			auto.setEstadoInicial("S0");
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		String lastEstate="S0";
+		
 		for (int i = 0; i < express.length(); i++) {
 			char at = express.charAt(i);
 			if (at == '(') {
@@ -60,9 +69,13 @@ public class Expressao {
 				if (end == -1) {
 					System.exit(-1);
 				}
-
+				
+				
 				String sub = express.substring(i + 1, end);
-				char submod = express.charAt(i + 1);
+				
+				char submod = '\0';
+				if(end< express.length()-1 )
+					submod = express.charAt(i + 1);
 
 				if (isMod(submod)) {
 					end++;
@@ -75,14 +88,96 @@ public class Expressao {
 				continue;
 			}
 			if (at == '|') {
+				/**
+				 * Caso encontrado um operador | é feito uniao
+				 * do automato atual com o restante da expressao
+				 */
 				String sub = express.substring(i + 1);
 
-				char submod = express.charAt(i + 1);
-
-				auto = auto.uniao(obterAutomato(sub, (char) 0));
+				return auto = auto.uniao( obterAutomato(sub, (char) 0) );
 			}
-
+			
+			/**
+			 * Verifica-se se ha algum modificador como *,+,? depois
+			 * do simbolo atual
+			 */
+			char cmod='\0';
+			if(i<express.length()+1)cmod=express.charAt( i+1 );
+			
+			if(isMod(cmod))i++;
+			
+			
+			String estadoAtual = lastEstate;
+			vn++;
+			String estadoTransicao = "S"+vn;
+			
+			auto.addEstado(estadoTransicao);		
+			String producao="";
+			producao += at;
+			
+			lastEstate = estadoTransicao;
+			
+			try {	
+			switch(cmod){
+				case '+':{
+					auto.addTransicao(estadoAtual , producao ,  estadoTransicao);
+					auto.addTransicao(estadoTransicao , producao ,  estadoTransicao);
+					break;
+				}
+				case'*':{
+					auto.addTransicao(estadoAtual , "&" ,  estadoTransicao);
+					auto.addTransicao(estadoTransicao , producao ,  estadoTransicao);
+					break;
+				}
+				case '?':{ 
+					auto.addTransicao(estadoAtual , producao ,  estadoTransicao);
+					auto.addTransicao(estadoAtual , "&" ,  estadoTransicao);
+					break;
+				}
+	
+				default:{
+					auto.addTransicao(estadoAtual , producao ,  estadoTransicao);
+					break;
+				}	
+			}
+			
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
+		
+		
+		/** Agora lidando com algum modificador que influencia em toda expressao
+		 * 
+		 */
+		String estadoInicial = "S0";
+		String estadoFinal = lastEstate;
+		
+		try {	
+			switch(mod){
+				case '+':{
+					auto.addTransicao(estadoFinal , "&" ,  estadoInicial);
+					break;
+				}
+				case'*':{
+					auto.addTransicao(estadoInicial , "&" ,  estadoFinal);
+					auto.addTransicao(estadoFinal , "&" ,  estadoInicial);
+					break;
+				}
+				case '?':{ 
+					auto.addTransicao(estadoInicial , "&" ,  estadoFinal);
+					break;
+				}
+	
+				default:{
+					
+					break;
+				}	
+			}
+			
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
 
 		return auto;
 	}
