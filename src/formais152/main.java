0089826de23067c71b1b1b778925e02912ccd796
Decker <luisgustavo.decker@gmail.com>
2015-09-30 09:39:02 -0300

    Criada a concatenação de automatos

--- src/formais152/main.java
+++ src/formais152/main.java
@@ -14,72 +14,105 @@ import formais152.Modelo.Gramatica;
  */
 public class main {
 
-	/**
-	 * @param args
-	 *            the command line arguments
-	 */
-	public static void main(String[] args) {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            Automato a = new Automato();
+            a.addSimbolo("a");
+            a.addSimbolo("b");
+            a.addEstado("q0");
+            a.addEstadoFinal("q1");
+            a.addEstadoFinal("q2");
+            a.addTransicao("q0", "a", "q1");
+            a.addTransicao("q0", "b", "q2");
+            a.setEstadoInicial("q0");
 
-		Automato aut = new Automato();
-		
-		/**testes rápidos que eu fiz, vou passar pro JUnit, pode ser apagado																																																																																																																						*/
-		
+            Automato b = new Automato();
+            b.addSimbolo("a");
+            b.addSimbolo("c");
+            b.addEstado("w0");
+            b.addEstado("w1");
+            b.addEstadoFinal("w2");
+            b.addTransicao("w0", "a", "w1");
+            b.addTransicao("w1", "c", "w2");
+            b.setEstadoInicial("w0");
+            System.out.println(a);
+            System.out.println(b);
 
-		aut.addEstado("S");
-		aut.addEstado("A");
-		aut.addEstado("B");
-		aut.addEstado("C");
-		aut.addEstadoFinal("S");
-		aut.addEstadoFinal("C");
+            Automato c = a.concatenacao(b);
+            System.out.println(c);
+            System.out.println(a);
+            System.out.println(b);
+            Automato d = b.concatenacao(a);
+            System.out.println(d);
+        } catch (Exception e) {
+
+        }
+        /*
+            
+            
+            
+         Automato aut = new Automato();
 		
-		try {
-			aut.setEstadoInicial("S");
-			aut.addTransicao("S", "a", "A");
-			aut.addTransicao("A", "a", "S");
-			aut.addTransicao("S", "b", "B");
-			aut.addTransicao("A", "b", "C");
-			aut.addTransicao("B", "b", "S");
-			aut.addTransicao("B", "a", "C");
-			aut.addTransicao("C", "a", "B");
-			aut.addTransicao("C", "b", "A");
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+         *testes rápidos que eu fiz, vou passar pro JUnit, pode ser apagado																																																																																																																						*/
+        /*
+
+         aut.addEstado("S");
+         aut.addEstado("A");
+         aut.addEstado("B");
+         aut.addEstado("C");
+         aut.addEstadoFinal("S");
+         aut.addEstadoFinal("C");
 		
-		System.out.println(aut);
+         try {
+         aut.setEstadoInicial("S");
+         aut.addTransicao("S", "a", "A");
+         aut.addTransicao("A", "a", "S");
+         aut.addTransicao("S", "b", "B");
+         aut.addTransicao("A", "b", "C");
+         aut.addTransicao("B", "b", "S");
+         aut.addTransicao("B", "a", "C");
+         aut.addTransicao("C", "a", "B");
+         aut.addTransicao("C", "b", "A");
+         } catch (Exception e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+         }
 		
-		Gramatica gr2 = aut.transformaEmGramatica();
-		gr2.retiraSimboloInicialDasProducoesADireita();
-//		System.out.println(gr2);
+         System.out.println(aut);
 		
-		System.out.println("\n");
+         Gramatica gr2 = aut.transformaEmGramatica();
+         gr2.retiraSimboloInicialDasProducoesADireita();
+         //		System.out.println(gr2);
 		
-		Gramatica gr = new Gramatica();
+         System.out.println("\n");
 		
-		gr.adicionaProducao("*S", "aA");
-		gr.adicionaProducao("S", "bB");
-		gr.adicionaProducao("S", "&");
-		gr.adicionaProducao("A", "aS");
-		gr.adicionaProducao("A", "bC");
-		gr.adicionaProducao("A", "a");
-		gr.adicionaProducao("A", "b");
-		gr.adicionaProducao("B", "bS");
-		gr.adicionaProducao("B", "aC");
-		gr.adicionaProducao("B", "a");
-		gr.adicionaProducao("B", "b");
-		gr.adicionaProducao("C", "aB");
-		gr.adicionaProducao("C", "bA");
+         Gramatica gr = new Gramatica();
 		
-		
-		gr.retiraSimboloInicialDasProducoesADireita();
-		
-		Automato aut2 = gr.transformaEmAutomato();
+         gr.adicionaProducao("*S", "aA");
+         gr.adicionaProducao("S", "bB");
+         gr.adicionaProducao("S", "&");
+         gr.adicionaProducao("A", "aS");
+         gr.adicionaProducao("A", "bC");
+         gr.adicionaProducao("A", "a");
+         gr.adicionaProducao("A", "b");
+         gr.adicionaProducao("B", "bS");
+         gr.adicionaProducao("B", "aC");
+         gr.adicionaProducao("B", "a");
+         gr.adicionaProducao("B", "b");
+         gr.adicionaProducao("C", "aB");
+         gr.adicionaProducao("C", "bA");
 		
 		
+         gr.retiraSimboloInicialDasProducoesADireita();
 		
-		System.out.println(aut2);
+         Automato aut2 = gr.transformaEmAutomato();
 		
+		
+		
+         System.out.println(aut2);*/
 //		System.out.println(gr);
 //		
 //		Automato aut2 = gr.transformaEmAutomato();
@@ -87,7 +120,6 @@ public class main {
 //		System.out.println(aut2);
 //		
 //		System.out.println(aut2.determinizar());
-
-	}
+    }
 
 }
