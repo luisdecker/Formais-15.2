4d110a7a090d50a3cff45d7986e409eb0f92f7cb
WagnerGascho <wgascho@uol.com.br>
2015-10-01 19:06:17 -0300

    Main update
    feito menu do main

--- src/formais152/main.java
+++ src/formais152/main.java
@@ -5,120 +5,151 @@
  */
 package formais152;
 
+import javax.swing.JOptionPane;
+
 import formais152.Modelo.Automato;
+import formais152.Modelo.Expressao;
 import formais152.Modelo.Gramatica;
+import formais152.Modelo.InputOutput;
 
-/**
- *
- * @author decker
- */
 public class main {
+	static String menu = "a) operações de automatos finito\nb) operações de expressão regular\nc) operações de gramaticas\n\nDigite a opção";
+	static String menuAutomato = "a)Ler automato\nb)determinizar\nc)Converter em gramatica\nd)Voltar";
+	static String menuExpressao = "a)Ler expressao\nb)transformar em Automato\nc)voltar";
+	static String menuGramatica = "a)Ler gramatica\nb)transformar em Automato\nc)voltar";
+	static Automato automato;
+	static Expressao expressao;
+	static Gramatica gramatica;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-       /* try {
-            System.out.println("AAAAAAAAA");
-            Automato a = new Automato();
-            a.addSimbolo("a");
-            a.addSimbolo("b");
-            a.addEstado("q0");
-            a.addEstadoFinal("q1");
-            a.addEstadoFinal("q2");
-            a.addTransicao("q0", "a", "q1");
-            a.addTransicao("q0", "b", "q2");
-            a.setEstadoInicial("q0");
+	static void automato() {
+		String text="";
+		while(text.length()==0){
+			text = JOptionPane.showInputDialog(menuAutomato);
+		}
+		char option = text.charAt(0);
 
-            Automato b = new Automato();
-            b.addSimbolo("a");
-            b.addSimbolo("c");
-            b.addEstado("w0");
-            b.addEstado("w1");
-            b.addEstadoFinal("w2");
-            b.addTransicao("w0", "a", "w1");
-            b.addTransicao("w1", "c", "w2");
-            b.setEstadoInicial("w0");
-            System.out.println(a);
-            System.out.println(b);
+		switch (option) {
+		case 'a': {
+			automato = InputOutput.criarAutomato("automato.in");
+			break;
+		}
+		case 'b': {
+			automato = automato.removerEpsilonTransicoes();
+			automato = automato.determinizar();
+			automato = automato.obterAutomatoMinimo();
 
-            Automato c = a.concatenacao(b);
-            System.out.println(c);
-            System.out.println(a);
-            System.out.println(b);
-            Automato d = b.concatenacao(a);
-            System.out.println(d);
-        } catch (Exception e) {
+			InputOutput.writeToFile(automato.toString(), "automato.out");
+			System.out.println(automato.toString());
+			break;
+		}
+		case 'c': {
+			gramatica = automato.transformaEmGramatica();
+			InputOutput.writeToFile(gramatica.toString(), "gramatica.out");
+			System.out.println(gramatica.toString());
 
-        }*/
-        
-            
-            
-            
-         Automato aut = new Automato();
-		
+			break;
+		}
+		case 'd': {
+			main(null);
+			return;
+		}
+		default:
+			break;
+		}
+		automato();
 
-         aut.addEstado("S");
-         aut.addEstado("A");
-         aut.addEstado("B");
-         aut.addEstado("C");
-         aut.addEstadoFinal("S");
-         aut.addEstadoFinal("C");
+	}
+
+	static void expressao() {
+		String text="";
+		while(text.length()==0){
+			text = JOptionPane.showInputDialog(menuExpressao);
+		}
+		char option = text.charAt(0);
+>>>>>>> master
 		
-         try {
-         aut.setEstadoInicial("S");
-         aut.addTransicao("S", "a", "A");
-         aut.addTransicao("A", "a", "S");
-         aut.addTransicao("S", "b", "B");
-         aut.addTransicao("A", "b", "C");
-         aut.addTransicao("B", "b", "S");
-         aut.addTransicao("B", "a", "C");
-         aut.addTransicao("C", "a", "B");
-         aut.addTransicao("C", "b", "A");
-         } catch (Exception e) {
-         // TODO Auto-generated catch block
-         e.printStackTrace();
-         }
+		switch (option) {
+		case 'a': {
+			expressao = InputOutput.criarExpressao("expressao.in");
+			break;
+		}
+		case 'b': {
+			try {
+				automato = expressao.obterAutomato();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			InputOutput.writeToFile(automato.toString(), "automato.out");
+			System.out.println(automato.toString());
+
+			break;
+		}
+		case 'c': {
+			 main(null);
+			 return;
+			
+		}
+		default:
+			break;
+		}
+
+		expressao();
+	}
+
+	static void gramatica() {
+		String text="";
+		while(text.length()==0){
+			text = JOptionPane.showInputDialog(menuGramatica);
+		}
+		char option = text.charAt(0);
 		
-         System.out.println(aut);
-		
-         Gramatica gr2 = aut.transformaEmGramatica();
-         gr2.retiraSimboloInicialDasProducoesADireita();
-         System.out.println(gr2);
-		
-         System.out.println("\n");
-		
-         Gramatica gr = new Gramatica();
-		
-         gr.adicionaProducao("*S", "aA");
-         gr.adicionaProducao("S", "bB");
-         gr.adicionaProducao("S", "&");
-         gr.adicionaProducao("A", "aS");
-         gr.adicionaProducao("A", "bC");
-         gr.adicionaProducao("A", "a");
-         gr.adicionaProducao("A", "b");
-         gr.adicionaProducao("B", "bS");
-         gr.adicionaProducao("B", "aC");
-         gr.adicionaProducao("B", "a");
-         gr.adicionaProducao("B", "b");
-         gr.adicionaProducao("C", "aB");
-         gr.adicionaProducao("C", "bA");
-		
-		
-         gr.retiraSimboloInicialDasProducoesADireita();
-		
-         Automato aut2 = gr.transformaEmAutomato();
-		
-		
-		
-  //       System.out.println(aut2);
-//		System.out.println(gr);
-//		
-//		Automato aut2 = gr.transformaEmAutomato();
-//		
-//		System.out.println(aut2);
-//		
-//		System.out.println(aut2.determinizar());
-    }
+		switch (option) {
+		case 'a': {
+			gramatica = InputOutput.criarGramatica("gramatica.in");
+			break;
+		}
+		case 'b': {
+			automato = gramatica.transformaEmAutomato();
+			InputOutput.writeToFile(automato.toString(), "automato.out");
+			System.out.println(automato.toString());
+			break;
+		}
+		case 'c': {
+			main(null);
+			return;
+		}
+		default:
+			break;
+		}
+		gramatica();
+
+	}
+
+	public static void main(String[] args) {
+		String text="";
+		while(text.length()==0){
+			text = JOptionPane.showInputDialog(menu);
+		}
+		char option = text.charAt(0);
+		switch (option) {
+		case 'a': {
+			automato();
+			break;
+		}
+		case 'b': {
+			expressao();
+			break;
+		}
+		case 'c': {
+			gramatica();
+			break;
+		}
+
+		default:
+			break;
+		}
+
+	}
 
 }
